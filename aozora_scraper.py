46a357615f2b7efb3ea4467755124d9cc634018e
sheepover96 <sheep.over@gmail.com>
2019-01-21 17:18:47 +0900

    error exception added

--- aozora_scraper.py
+++ aozora_scraper.py
@@ -41,55 +41,62 @@ if __name__ == '__main__':
     toppage_soup = bs(aozora_top, 'html.parser')
     idx_link_list = toppage_soup.find_all(href=re.compile('person'))
 
-    # get author list
-    for idx_link in idx_link_list:
-        with urllib.request.urlopen(ROOT_URL + idx_link.get('href')) as response:
-            author_list = response.read()
-        time.sleep(SLEEP_SECONDS)
-        author_list_soup = bs(author_list, 'html.parser')
-        author_link_list = author_list_soup.find_all(href=re.compile('person[0-9]+'))
-        # get novel list
-        for author_link in author_link_list:
-            with urllib.request.urlopen(ROOT_URL + MIDDLE_URI + author_link.get('href')) as response:
+    try:
+        # get author list
+        for idx_link in idx_link_list:
+            with urllib.request.urlopen(ROOT_URL + idx_link.get('href')) as response:
                 author_list = response.read()
             time.sleep(SLEEP_SECONDS)
             author_list_soup = bs(author_list, 'html.parser')
-            novel_link_list = author_list_soup.find_all(href=re.compile('person[0-9]+'))
-            # get novel detail
-            for novel_link in novel_link_list:
+            author_link_list = author_list_soup.find_all(href=re.compile('person[0-9]+'))
+            # get novel list
+            for author_link in author_link_list:
                 with urllib.request.urlopen(ROOT_URL + MIDDLE_URI + author_link.get('href')) as response:
-                    novel_detail_list = response.read()
+                    author_list = response.read()
                 time.sleep(SLEEP_SECONDS)
-                novel_detail_list_soup = bs(novel_detail_list, 'html.parser')
-                novel_detail_link_list = novel_detail_list_soup.find_all(href=re.compile('card[0-9]+'))
-                # download novel
-                for novel_detail_link in novel_detail_link_list:
-                    novel_detail_complete_link = ROOT_URL + novel_detail_link.get('href')[3:]
-                    with urllib.request.urlopen(novel_detail_complete_link) as response:
-                        novel_detail = response.read()
+                author_list_soup = bs(author_list, 'html.parser')
+                novel_link_list = author_list_soup.find_all(href=re.compile('person[0-9]+'))
+                # get novel detail
+                for novel_link in novel_link_list:
+                    with urllib.request.urlopen(ROOT_URL + MIDDLE_URI + author_link.get('href')) as response:
+                        novel_detail_list = response.read()
                     time.sleep(SLEEP_SECONDS)
-                    # access to xhtml
-                    novel_detail_soup = bs(novel_detail, 'html.parser')
-                    novel_detail_parent = remove_tail_filename(novel_detail_complete_link, '/')
-                    novel_link = novel_detail_soup.find(href=re.compile('\./files.+html'))
-                    if novel_link is not None:
-                        novel_html_complete_link = novel_detail_parent + novel_link.get('href')[2:]
-                        with urllib.request.urlopen(novel_html_complete_link) as response:
-                            novel_html = response.read()
+                    novel_detail_list_soup = bs(novel_detail_list, 'html.parser')
+                    novel_detail_link_list = novel_detail_list_soup.find_all(href=re.compile('card[0-9]+'))
+                    # download novel
+                    for novel_detail_link in novel_detail_link_list:
+                        novel_detail_complete_link = ROOT_URL + novel_detail_link.get('href')[3:]
+                        with urllib.request.urlopen(novel_detail_complete_link) as response:
+                            novel_detail = response.read()
                         time.sleep(SLEEP_SECONDS)
-                        try:
-                            novel_html_soup = bs(novel_html, 'html.parser')
-                            novel_title = novel_html_soup.find('h1', class_='title')
-                            novel_author = novel_html_soup.find('h2', class_='author')
-                            novel_content = novel_html_soup.find('div', class_='main_text')
-                            print(novel_title.string)
-                            print(novel_author.string)
-                            make_dir('novels/' + novel_author.string)
-                            save_novel('novels/' + novel_author.string + '/' + novel_title.string, novel_content.text)
-                            #print(novel_content.text)
-                        except AttributeError as e:
-                            print(e)
-                            print(novel_link)
-                        except urllib.error.URLError as e2:
-                            print(e)
-                            print(novel_link)
+                        # access to xhtml
+                        novel_detail_soup = bs(novel_detail, 'html.parser')
+                        novel_detail_parent = remove_tail_filename(novel_detail_complete_link, '/')
+                        novel_link = novel_detail_soup.find(href=re.compile('\./files.+html'))
+                        if novel_link is not None:
+                            novel_html_complete_link = novel_detail_parent + novel_link.get('href')[2:]
+                            with urllib.request.urlopen(novel_html_complete_link) as response:
+                                novel_html = response.read()
+                            time.sleep(SLEEP_SECONDS)
+                            try:
+                                novel_html_soup = bs(novel_html, 'html.parser')
+                                novel_title = novel_html_soup.find('h1', class_='title')
+                                novel_author = novel_html_soup.find('h2', class_='author')
+                                novel_content = novel_html_soup.find('div', class_='main_text')
+                                print(novel_title.string)
+                                print(novel_author.string)
+                                make_dir('novels/' + novel_author.string)
+                                save_novel('novels/' + novel_author.string + '/' + novel_title.string, novel_content.text)
+                                #print(novel_content.text)
+                            except AttributeError as e:
+                                print(e)
+                                print(novel_link)
+                            except urllib.error.URLError as e2:
+                                print(e2)
+                                print(novel_link)
+                            except OSError as e3:
+                                print(e3)
+                                print(novel_link)
+    except:
+        import traceback
+        traceback.print_exc()
