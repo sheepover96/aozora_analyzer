2d2741201f7f74542ef7e555a4c4dc12b8d91548
sheepover96 <sheep.over@gmail.com>
2019-04-28 19:00:08 +0900

    embedding check

--- aozora_vec.py
+++ aozora_vec.py
@@ -6,7 +6,8 @@ from utils.pickle_helper import pickle_load, pickle_dump
 from utils.tokenizers import tokenizer
 
 DATASET_DIR = './novels'
-MODEL_PATH = './pickle_objects/aozora_w2v.pickle'
+MODEL_PATH = './pickle_objects/aozora_w2v_nonormalize.pickle'
+MODEL_PATH2 = './pickle_objects/aozora_w2v2_nonormalize.model'
 
 def main():
     novel2idx = pickle_load('./pickle_objects/novel2idx.pickle')
@@ -15,15 +16,14 @@ def main():
     for author in novel2idx.keys():
         for novel in novel2idx[author]:
             with open(os.path.join(DATASET_DIR, author, novel), 'r') as f:
-                print(author)
                 novel_content = f.read()
                 novel_text_lines = novel_content.split('\n')
-                print(type(novel_text_lines))
-                tokenized_novel_text_lines = [ tokenizer(text_line, part_use=None) for text_line in novel_text_lines]
-                all_novel_lines.append(tokenized_novel_text_lines)
-    
-    model = word2vec.Word2Vec(all_novel_lines, iter=2)
+                tokenized_novel_text_lines = [ tokenizer(text_line, part_use=None, normalize_word=False) for text_line in novel_text_lines]
+                all_novel_lines.extend(tokenized_novel_text_lines)
+    print('training')
+    model = word2vec.Word2Vec(all_novel_lines, iter=100)
     pickle_dump(model, MODEL_PATH)
+    model.save(MODEL_PATH2)
 
 
 if __name__ == '__main__':
--- utils/tokenizers.py
+++ utils/tokenizers.py
@@ -6,7 +6,7 @@ with open('stop_words.txt', 'r') as f:
     stopwords = [word for word in content.split('\n')]
 stopwords.extend(hiragana)
 
-def tokenizer(words, part_use=['名詞', '形容詞']):
+def tokenizer(words, part_use=['名詞', '形容詞'], normalize_word=True):
     tagger = mecab.Tagger('-d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd ')
     tagger.parse('')
     mecab_word_nodes = tagger.parseToNode(words)
@@ -15,14 +15,15 @@ def tokenizer(words, part_use=['名詞', '形容詞']):
         elements = mecab_word_nodes.feature
         word = mecab_word_nodes.surface
         element_list = elements.split(',')
-        gen_word = element_list[6]
+        if normalize_word:
+            word = element_list[6]
         part = element_list[0]
 
-        if not gen_word in stopwords: 
+        if not word in stopwords:
             if part is None or part_use is None:
-                tokenized.append(gen_word)
-            elif part in part_use and not gen_word.encode('utf-8').isalnum():
-                tokenized.append(gen_word)
+                tokenized.append(word)
+            elif part in part_use and not word.encode('utf-8').isalnum():
+                tokenized.append(word)
 
         mecab_word_nodes = mecab_word_nodes.next
 
